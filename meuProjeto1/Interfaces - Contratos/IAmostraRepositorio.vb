67c0261e178c639b45cb5c287503309f7440dc5a
nathanfernandesalvim <nathan_alvim@yahoo.com.br>
2025-07-08 22:54:44 -0300

    v.01 - classe interface adicionada

---
+++ meuProjeto1/Interfaces - Contratos/IAmostraRepositorio.vb
@@ -0,0 +1,13 @@
+﻿Imports meuProjeto1.Models
+
+Namespace meuProjeto1.Interfaces
+
+    Public Interface IAmostraRepositorio
+        Sub Inserir(amostra As Amostra)
+        Sub Atualizar(amostra As Amostra)
+        Sub Deletar(id As Integer)
+        Function LerAmostra(id As Integer) As Amostra
+        Function ObterPrimeiroRegistro() As Amostra
+    End Interface
+
+End Namespace
